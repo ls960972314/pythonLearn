0860e15ea00ddb6bfa95245a978b980816d5339a
ls960972314 <960972314@qq.com>
2017-07-05 18:57:29 +0800

    python baseGrammar python基础语法

---
+++ base/variable.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+print """
+创建一个整型对象，值为1，三个变量被分配到相同的内存空间上
+"""
+a = b = c = 1
+
+print """
+两个整型对象1和2的分配给变量 a 和 b，字符串对象 john 分配给变量 c
+"""
+a, b, c = 1, 2, "john"
+
+print """
+python数字是不可改变的数据类型，这意味着改变数字数据类型会分配一个新的对象
+"""
+var1 = 1
+var2 = 10
+
+print """
+使用del语句删除一些对象的引用
+"""
+del var1, var2
+
+print """
+字符串或串(String)是由数字、字母、下划线组成的一串字符
+"""
+str = 'Hello World!'
+
+print str  # 输出完整字符串
+print str[0]  # 输出字符串中的第一个字符
+print str[2:5]  # 输出字符串中第三个至第五个之间的字符串
+print str[2:]  # 输出从第三个字符开始的字符串
+print str * 2  # 输出字符串两次
+print str + "TEST"  # 输出连接的字符串
+
+print """
+List（列表） 是 Python 中使用最频繁的数据类型。
+列表可以完成大多数集合类的数据结构实现。它支持字符，数字，字符串甚至可以包含列表（即嵌套）。
+列表用 [ ] 标识，是 python 最通用的复合数据类型。
+列表中值的切割也可以用到变量 [头下标:尾下标] ，就可以截取相应的列表，从左到右索引默认 0 开始，从右到左索引默认 -1 开始，下标可以为空表示取到头或尾。
+加号 + 是列表连接运算符，星号 * 是重复操作。如下实例：
+"""
+list = ['runoob', 786, 2.23, 'john', 70.2]
+tinylist = [123, 'john']
+
+print list  # 输出完整列表
+print list[0]  # 输出列表的第一个元素
+print list[1:3]  # 输出第二个至第三个的元素
+print list[2:]  # 输出从第三个开始至列表末尾的所有元素
+print tinylist * 2  # 输出列表两次
+print list + tinylist  # 打印组合的列表
+
+print """
+元组是另一个数据类型，类似于List（列表）。
+元组用"()"标识。内部元素用逗号隔开。但是元组不能二次赋值，相当于只读列表。
+"""
+print "python元组"
+tuple = ('runoob', 786, 2.23, 'john', 70.2)
+tinytuple = (123, 'john')
+
+print tuple  # 输出完整元组
+print tuple[0]  # 输出元组的第一个元素
+print tuple[1:3]  # 输出第二个至第三个的元素
+print tuple[2:]  # 输出从第三个开始至列表末尾的所有元素
+print tinytuple * 2  # 输出元组两次
+print tuple + tinytuple  # 打印组合的元组
+
+print '''
+字典(dictionary)是除列表以外python之中最灵活的内置数据结构类型。列表是有序的对象结合，字典是无序的对象集合。
+两者之间的区别在于：字典当中的元素是通过键来存取的，而不是通过偏移存取。
+字典用"{ }"标识。字典由索引(key)和它对应的值value组成。
+'''
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+
+tinydict = {'name': 'john', 'code': 6734, 'dept': 'sales'}
+
+print dict['one']  # 输出键为'one' 的值
+print dict[2]  # 输出键为 2 的值
+print tinydict  # 输出完整的字典
+print tinydict.keys()  # 输出所有键
+print tinydict.values()  # 输出所有值
